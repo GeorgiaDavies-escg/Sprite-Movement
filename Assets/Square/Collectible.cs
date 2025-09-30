695a335c1db5296a559a4d91fac0119cb1ceb289
GeorgiaDavies-escg <535269@escg.ac.uk>
2025-09-30 12:08:29 +0100

    Consumeables

---
+++ Assets/Square/Collectible.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
